a68b86f482fadb124857544f6442afb7d7cec854
Mike Marynowski <mikem@singulink.com>
2025-12-26 11:10:02 -0500

    Add project load warnings, optional export names

--- IconPackBuilder/IconPackBuilder.Core/Services/ExportIconInfo.cs
+++ IconPackBuilder/IconPackBuilder.Core/Services/ExportIconInfo.cs
@@ -1,3 +1,6 @@
 namespace IconPackBuilder.Core.Services;
 
-public record ExportIconInfo(string ExportName, IconInfo Icon);
+public record ExportIconInfo(string ExportName, IconInfo Icon)
+{
+    public string ExportName { get; init; } = string.IsNullOrWhiteSpace(ExportName) ? Icon.Group.Id : ExportName;
+}
