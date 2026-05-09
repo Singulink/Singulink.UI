0e6ff68efb595b174252d8b24c9e219470285086
Mike Marynowski <mikem@singulink.com>
2026-05-09 15:47:31 -0400

    Fix playground nav sync with system nav requests

--- Playground/Playground.ViewModels/MainViewModel.cs
+++ Playground/Playground.ViewModels/MainViewModel.cs
@@ -20,6 +20,27 @@ public partial class MainViewModel : ObservableObject, IRoutedViewModel, IMessag
     [ObservableProperty]
     public partial MenuItem SelectedMenuItem { get; set; } = new("", null);
 
+    public async Task OnNavigatedToAsync(NavigationArgs args)
+    {
+        this.SetChildService(new MessageContainer("Hello from MainViewModel via child service MessageContainer!"));
+        await Task.Delay(1000);
+    }
+
+    public Task OnRouteNavigatedAsync(NavigationArgs args)
+    {
+        // If there is no child navigation (because a view routed directly to "/" instead of "/Home", for example),
+        // navigate to home.
+
+        if (args.HasChildNavigation)
+            SelectedMenuItem = MainMenuItems.First(mi => Navigator.CurrentPathStartsWith(Routes.MainRoot, mi.ChildRoutePart!));
+        else
+            args.Redirect = Redirect.NavigatePartial(MainMenuItems[0].ChildRoutePart!);
+
+        return Task.CompletedTask;
+    }
+
+    public void BeginBackRequest() => this.TaskRunner.RunAndForget(Navigator.GoBackAsync);
+
     partial void OnSelectedMenuItemChanged(MenuItem oldValue, MenuItem newValue)
     {
         if (Navigator.IsNavigating || Navigator.CurrentRoute.Parts.Contains(newValue.ChildRoutePart))
@@ -42,29 +63,6 @@ public partial class MainViewModel : ObservableObject, IRoutedViewModel, IMessag
                 SelectedMenuItem = oldValue;
         });
     }
-
-    public async Task OnNavigatedToAsync(NavigationArgs args)
-    {
-        this.SetChildService(new MessageContainer("Hello from MainViewModel via child service MessageContainer!"));
-        SelectedMenuItem = MainMenuItems[0];
-        await Task.Delay(1000);
-    }
-
-    public Task OnRouteNavigatedAsync(NavigationArgs args)
-    {
-        // If there is no child navigation (because a view routed directly to "/" instead of "/Home", for example),
-        // navigate to the selected menu item (which is Home by default).
-
-        if (!args.HasChildNavigation)
-            args.Redirect = Redirect.NavigatePartial(SelectedMenuItem.ChildRoutePart!);
-
-        return Task.CompletedTask;
-    }
-
-    public void BeginBackRequest() => this.TaskRunner.RunAndForget(async () => {
-        await Navigator.GoBackAsync();
-        SelectedMenuItem = MainMenuItems.First(mi => Navigator.CurrentPathStartsWith(Routes.MainRoot, mi.ChildRoutePart!));
-    });
 
     string IMessageProvider.GetMessage() => "Hello from MainViewModel via direct IMessageProvider!";
 
